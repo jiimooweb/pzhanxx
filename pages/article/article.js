bfe045cad6370528138188d266a9c6b38176d19a
翻水水 <wx_2e929a573f6147d0b059920bc5907022@git.code.tencent.com>
2018-11-16 22:15:43 +0800

    commit

---
+++ pages/article/article.js
@@ -0,0 +1,47 @@
+import { Config } from '../../utils/config.js';
+import { Token } from "../../utils/token.js"
+var WxParse = require('../../wxParse/wxParse.js');
+
+const token = new Token
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    article: [],
+    id: 0
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.setData({
+      id: options.id
+    })
+    token.verify(this.getArticle)
+  },
+
+  getArticle: function() {
+    var id = this.data.id
+    wx.showLoading({
+      title: '加载中',
+    })
+    wx.request({
+      url: Config.restUrl + '/articles/' + 1,
+      method: 'GET',
+      header: {'token': wx.getStorageSync('token')},
+      success: res => {
+        var article = res.data.data
+        var that = this;
+        WxParse.wxParse('wxParse', 'html', article.content, that, 20);
+        that.setData({
+          article: article
+        })
+        wx.hideLoading()
+      }
+    })
+  }
+})
